f20b7b16db44e10a3de7fb92ae323e140524029c
Namirah0310 <nare0001@stud.ek.dk>
2025-09-24 11:17:32 +0200

    commit03

--- src/Spotify/SpotifyTest.java
+++ src/Spotify/SpotifyTest.java
@@ -11,6 +11,7 @@ public class SpotifyTest {
     //starts the program
     public static void welcome(){
         System.out.println("Welcome to the homemade Spotify. Choose what to do:");
+        // make different options for what to do.
     }
     //can be called to display the songs
     /* public static void displaySongs(){
