0dcb28de51cf8f7a640d64662aeac16f06d99b10
jacobvad1337 <jacobvad@Aiwa.local>
2025-09-24 10:39:04 +0200

    song

--- src/Spotify/Song.java
+++ src/Spotify/Song.java
@@ -1,4 +1,28 @@
 package Spotify;
 
 public class Song {
+    private String title;
+    private Genre genre;
+
+    //Constructor
+    public Song(String tilte, Genre genre){
+        this.title = title;
+        this.genre = genre;
+    }
+    //Getters
+    public String getTitle(){
+        return title;
+    }
+    public Genre getGenre() {
+        return genre
+    }
+    //Setters
+    public void setTitle(String title){this.title = title; }
+    public void setGenre(Genre genre){this.genre = genre; }
+
+    //toString method
+    @Override
+    public String toString(){
+        return title + "---" + genre;
+    }
 }
